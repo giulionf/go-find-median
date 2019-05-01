781ea9999253217fca48ab345268ca59e17b45bf
Giulio <giulio.nf@googlemail.com>
2019-05-01 16:39:21 +0200

    Added Plotting of the runtimes

--- main.go
+++ main.go
@@ -2,51 +2,82 @@ package main
 
 import (
 	"fmt"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
+	"gonum.org/v1/plot/vg"
 	"median-finding/algorithms"
 	"median-finding/util"
 	"time"
 )
 
+const(
+	// Labels + File name of the plot generated
+	PlotTitle = "Laufzeitvergleich"
+	PlotXAxisLabel = "n"
+	PlotYAxisLabel = "Laufzeit in Sekunden"
+	LabelLinearSelection = "Repeated Linear Selection"
+	LabelSortFirst = "Sort First"
+	LabelQuickSelect = "QuickSelect"
+	LabelMedianOfMedians = "Median of Medians"
+
+	// File the plot will be saved to
+	PlotFile = "comparison.png"
+
+	// Step for each point in the x axis of the plot
+	PlotStep = 1000
+
+	// Total Length of the random list
+	ListLength = 30000
+)
+
 func main() {
-	list := util.GenerateRandomList(10000000)
-	fmt.Println("---- RANDOM LIST ----")
-	//iteration(list)
-	sortfirst(list)
-	divideAndConquer(list)
-	fast(list)
-
-	list2 := util.GenerateRandomList(10000000)
-	fmt.Println("\n---- INCREMENTING LIST ----")
-	//iteration(list)
-	sortfirst(list2)
-	divideAndConquer(list2)
-	fast(list2)
+	list := util.GenerateRandomList(ListLength)
+	makePlot(list)
 }
 
-func iteration(list []int) {
-	startTime := time.Now()
-	algorithms.FindMedian(algorithms.RepeatedLinearSelection, list)
-	elapsedTime := time.Since(startTime)
-	fmt.Printf("%s executed within: %s\n", "Iteration", elapsedTime)
+func makePlot(list []int) {
+	p, err := plot.New()
+	if err != nil {
+		panic(err)
+	}
+
+	p.Title.Text = PlotTitle
+	p.X.Label.Text = PlotXAxisLabel
+	p.Y.Label.Text = PlotYAxisLabel
+
+	err = plotutil.AddLinePoints(p,
+		LabelLinearSelection, getPoints(algorithms.RepeatedLinearSelection, PlotStep, list),
+		LabelSortFirst, getPoints(algorithms.SortFirst, PlotStep, list),
+		LabelQuickSelect, getPoints(algorithms.QuickSelect, PlotStep, list),
+		LabelMedianOfMedians, getPoints(algorithms.MedianOfMedians, PlotStep, list))
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := p.Save(6*vg.Inch, 6*vg.Inch, PlotFile); err != nil {
+		panic(err)
+	}
 }
 
-func sortfirst(list []int) {
-	startTime := time.Now()
-	algorithms.FindMedian(algorithms.SortFirst, list)
-	elapsedTime := time.Since(startTime)
-	fmt.Printf("%s executed within: %s\n", "SortFirst", elapsedTime)
-}
+func getPoints(algorithm algorithms.Algorithm, step int, list []int) plotter.XYs {
+	fmt.Print("STARTING COLLECTING PLOTS FOR ALGORITHM ", algorithm)
+	points := make(plotter.XYs, len(list) / step)
 
-func divideAndConquer(list []int) {
-	startTime := time.Now()
-	algorithms.FindMedian(algorithms.QuickSelect, list)
-	elapsedTime := time.Since(startTime)
-	fmt.Printf("%s executed within: %s\n", "Divide And Conquer", elapsedTime)
-}
+	for i := 1; i < len(points); i++ {
+		// Debug the percentage
+		fmt.Println((step*i*100)/len(list), "%")
 
-func fast(list []int) {
-	startTime := time.Now()
-	algorithms.FindMedian(algorithms.MedianOfMedians, list)
-	elapsedTime := time.Since(startTime)
-	fmt.Printf("%s executed within: %s\n", "MedianOfMedians", elapsedTime)
+		// Measure the time for the sub list
+		slice := list[:i*step]
+		startTime := time.Now()
+		algorithms.FindMedian(algorithm, slice)
+		elapsedTime := time.Since(startTime)
+
+		// Add the measurements to the plot
+		points[i].X = float64(i*step)
+		points[i].Y = float64(elapsedTime.Seconds())
+	}
+	return points
 }
